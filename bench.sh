e22b34c6dcb6981dfe3e0288b4b0ed9b8b7c1b96
Christoph Herzog <chris@theduke.at>
2021-03-09 22:08:00 +0100

    add benchmark

--- bench.sh
+++ bench.sh
@@ -8,14 +8,13 @@ if [[ ! -f $cg_clif_dir/build/cargo.sh ]]; then
 fi
 
 echo "llvm without lld"
-hyperfine -w 5 'touch src/main.rs && CARGO_TARGET_DIR=target/llvm cargo build'
-
-echo "llvm with lld"
-hyperfine -w 5 'touch src/main.rs && CARGO_TARGET_DIR=target/llvm_lld RUSTFLAGS="-C link-arg=-fuse-ld=lld" cargo build'
-
-echo "clif without lld"
-hyperfine -w 5 'touch src/main.rs && CARGO_TARGET_DIR=target/clif $cg_clif_dir/build/cargo.sh build'
-
-echo "clif with lld"
-hyperfine -w 5 'touch src/main.rs && CARGO_TARGET_DIR=target/clif_lld RUSTFLAGS="-C link-arg=-fuse-ld=lld" $cg_clif_dir/build/cargo.sh build'
-
+hyperfine -w 5 \
+  --export-csv timings.csv \
+  -n llvm-no-lld \
+  'touch src/main.rs && CARGO_TARGET_DIR=target/llvm cargo build' \
+  -n llvm-with-lld \
+  'touch src/main.rs && CARGO_TARGET_DIR=target/llvm_lld RUSTFLAGS="-C link-arg=-fuse-ld=lld" cargo build' \
+  -n clif-no-lld \
+  'touch src/main.rs && CARGO_TARGET_DIR=target/clif $cg_clif_dir/build/cargo.sh build' \
+  -n clif-with-lld \
+  'touch src/main.rs && CARGO_TARGET_DIR=target/clif_lld RUSTFLAGS="-C link-arg=-fuse-ld=lld" $cg_clif_dir/build/cargo.sh build'
